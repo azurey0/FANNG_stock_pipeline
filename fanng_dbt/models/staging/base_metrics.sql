866585f38cfb2bbba3cac878920a1b89ea01435e
azurey0 <923504994@qq.com>
2024-04-12 00:19:25 +0000

    feat: add dbt to project

---
+++ fanng_dbt/models/staging/base_metrics.sql
@@ -0,0 +1,29 @@
+{{ config(
+    materialized='incremental',
+    alias='base_metrics',
+    partition_by={
+        'field': '_PARTITIONTIME',
+        'data_type': 'timestamp',
+        'granularity': 'day'
+    },
+    incremental_strategy='insert_overwrite',
+    full_refresh = false
+) }}
+
+SELECT
+    CURRENT_TIMESTAMP() as _PARTITIONTIME,  -- This generates the ingestion timestamp
+    DATE(parse_date('%Y-%m-%d', Date)) as date,
+    SAFE_CAST(Open as NUMERIC) as open,
+    SAFE_CAST(High as NUMERIC) as high,
+    SAFE_CAST(Low as NUMERIC) as low,
+    SAFE_CAST(Close as NUMERIC) as close,
+    SAFE_CAST(Volume as NUMERIC) as volume,
+    Company as company
+FROM
+    {{ source('fanng_stock', 'raw') }}
+
+-- Specify how dbt decides what data to insert on each run
+{% if is_incremental() %}
+  -- this filter will only be applied on incremental runs
+  WHERE date_column > (SELECT MAX(date_column) FROM {{ this }})
+{% endif %}
